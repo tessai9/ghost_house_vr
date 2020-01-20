0f9b954209dd51e019f6081275ed4cd58b2219ef
tessai9 <shimeji9@gmail.com>
2020-01-20 21:44:44 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import 'aframe'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = [
+  'a-scene',
+  'a-assets',
+  'a-camera',
+  'a-entity',
+]
 
 new Vue({
   render: h => h(App),
