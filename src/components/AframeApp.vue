9b2dcc50411d3a52c6dbc706e45545d49a19cb5a
biwamin <atuto.biwa1123@gmail.com>
2020-01-28 21:09:13 +0900

    Player Componentの作成

---
+++ src/components/AframeApp.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <a-scene>
+    <a-assets>
+      <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg">
+      <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg">
+    </a-assets>
+    <a-cylinder id="ground" src="#groundTexture" radius="30" height="0.1"></a-cylinder>
+    <a-sky id="background" src="#skyTexture" theta-length="90" radius="30"></a-sky>
+
+    <Player :position="position"/>
+  </a-scene>
+</template>
+
+<script>
+import Player from './Player.vue'
+
+export default {
+  name: 'AframeApp',
+  data() {
+    return {
+      position: {x:0, y:0, z:1}
+    }
+  },
+  components: {
+    Player,
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,84 @@
+<template lang="html">
+    <a-entity 
+        id="rig" 
+        :position="cameraPosition"
+        @mouseup="onClick"
+    >
+        <a-camera
+            id="camera"
+            look-controls
+            wasd-controls="acceleration: 1000" 
+            rotation-reader>
+            <a-entity cursor="fuse: true; fuseTimeout: 50"
+                position="0 0 -1"
+                geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
+                material="color: red; shader: flat"
+                cursor-listener>
+            </a-entity>
+        </a-camera>
+    </a-entity>
+</template>
+
+<script>
+    import aframe from "aframe"
+    // cameraの位置情報を逐次取得
+    aframe.registerComponent('rotation-reader', {
+        tick: function () {
+            // `this.el` is the element.
+            // `object3D` is the three.js object.
+
+            // `rotation` is a three.js Euler using radians. `quaternion` also available.
+            //console.log(this.el.object3D.rotation);
+
+            // `position` is a three.js Vector3.
+            //console.log(this.el.object3D.position);
+        }
+    });
+
+    // クリックでカーソルの色を変更(aframeのentityにイベント付与)
+    aframe.registerComponent('cursor-listener', {
+        init: function () {
+                var lastIndex = -1;
+                var COLORS = ['red', 'green', 'blue'];
+                this.el.addEventListener('mouseup', function (evt) {
+                    lastIndex = (lastIndex + 1) % COLORS.length;
+                    this.setAttribute('material', 'color', COLORS[lastIndex]);
+                    console.log('I was clicked at: ', evt.detail.intersection.point);
+            });
+        }
+    });
+
+    export default {
+        name: 'Player',
+        props: {
+            position: {
+                type: Object,
+                required: false,
+                default: () => ( {x:0, y: 0, z:1} )
+            }
+        },
+        data() {
+            return {
+                localPosition: {
+                    x : this.position.x,
+                    y : this.position.y,
+                    z : this.position.z
+                }
+            }
+        },
+        computed: {
+            cameraPosition () {
+                const pos = this.localPosition
+                return `${pos.x} ${pos.y} ${pos.z}`
+            }
+        },
+        methods: {
+            onClick() {
+                this.localPosition.y += 0.1
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+</style>
