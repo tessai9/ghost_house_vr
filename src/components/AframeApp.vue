31e70d7f8f2bb2941dab283005d2b87f5b17167f
tesao <shimeji9@gmail.com>
2020-02-01 17:15:31 +0900

    地面の追加とお化け画像の差し替え

--- src/components/AframeApp.vue
+++ src/components/AframeApp.vue
@@ -1,11 +1,22 @@
 <template lang="html">
   <a-scene>
-    <!-- component化が必要？ -->
+    <!-- Need to be component? -->
     <a-assets>
-      <img id="sample_ghost" src="@/assets/animal_hone_hitsuji_zugaikotsu.png" />
+      <img id="ghost_woman" src="@/assets/ghost_woman.png">
+      <img id="ground_img" src="@/assets/ground.jpg">
     </a-assets>
 
-    <Ghost img_path="#sample_ghost" :cordinate="cordinate" :size="size" isDisplay="true" />
+    <Ghost img_path="#ghost_woman" :cordinate="cordinate" :size="size" isDisplay="true" />
+
+    <!-- Will be component in the future -->
+    <a-entity
+      v-for="(item, index) in ground_map"
+      :key="index"
+      geometry="primitive: plane; width: 3; height: 1;"
+      material="src: #ground_img"
+      rotation="-90 0 0"
+      :position="setGroundPosition(index)"
+    ></a-entity>
   </a-scene>
 </template>
 
@@ -19,13 +30,23 @@ export default {
       // demo property data for ghost component
       cordinate: {
         x: 0,
-        y: 1,
+        y: 2,
         z: -5
       },
       size: {
         width: 2,
-        height: 2
-      }
+        height: 3
+      },
+      // map information
+      ground_map: ["gr", "gr", "gr", "gr", "gr", "gr", "gr", "gr", "gr", "gr", "gr"],
+      z_move: 1,
+    }
+  },
+  methods: {
+    // Set Ground Position
+    setGroundPosition(index) {
+      const z_position = index > 0 ? index * this.z_move * -1 : index;
+      return { x: 0, y: 0, z: z_position }
     }
   },
   components: {
